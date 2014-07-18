3563f48d3caf20dfbe96174c5547bb858e7b8032
trifonovkv <trifonovkv@gmail.com>
2014-07-18 13:52:22 +0600

    Add triangular pointers

--- extension.js
+++ extension.js
@@ -6,8 +6,21 @@ const Mainloop=imports.mainloop;
 const Clutter=imports.gi.Clutter;
 const PanelMenu=imports.ui.panelMenu;
 const PopupMenu=imports.ui.popupMenu;
+const JSUnit = imports.jsUnit;
 
-let url = 'http://quotes.instaforex.com/get_quotes.php';
+const QUOTES_URL = 'http://quotes.instaforex.com/get_quotes.php';
+const UP_POINTING = String.fromCharCode(9650);
+const DOWN_POINTING = String.fromCharCode(9660);
+
+const Quote = new Lang.Class({
+	Name: 		'Quote',
+	symbol:		null,
+	ask:		0.0,
+	bid:		0.0,
+	change: 	0.0,
+	digits:		0,
+	lasttime:	0,
+})
 
 const ForexIndicator = new Lang.Class({
 	Name: 'ForexIndicator',
@@ -21,6 +34,7 @@ const ForexIndicator = new Lang.Class({
 		this.parent(0.0,"Forex Indicator",false);
 		this.buttonText=new St.Label({
          	name: "forex-indicator-buttonText",
+			text: "Loading...",
          	y_align: Clutter.ActorAlign.CENTER
      	});
 		this.actor.add_actor(this.buttonText);
@@ -96,27 +110,46 @@ const ForexIndicator = new Lang.Class({
 			this.refresh();
 		}));
 		this.menu.addMenuItem(item);
+
+		let item = new PopupMenu.PopupSeparatorMenuItem();
+        this.menu.addMenuItem(item);
+
+		let item = new PopupMenu.PopupMenuItem("Close");
+        item.connect('activate', Lang.bind(this, function(){
+			this.destroy();
+		}));
+		this.menu.addMenuItem(item);
 		
 		this.change_timeoutloop=true;
 		this.refresh();
 	},
 	
-	load_quote: function(pair, cb) {
+	load_quote: function(pair, callback) {
 		let params = {m: 'json', q: null};
 		params.q=pair;
 		const _httpSession = new Soup.Session();
-  		let message = Soup.form_request_new_from_hash('GET', url, params);
+  		let message = Soup.form_request_new_from_hash('GET', QUOTES_URL, params);
 		_httpSession.queue_message(message, Lang.bind(this, function(_httpSession, message) { 
          	let json = JSON.parse(message.response_body.data);
             for (let i in json) {
-				cb(json[i].bid);
+				current_quote.symbol = json[i].symbol;
+				current_quote.ask = json[i].ask;
+				current_quote.bid = json[i].bid;
+				current_quote.change = json[i].change;
+				current_quote.digits = json[i].digits;
+				current_quote.lasttime = json[i].lastime;				
 			}
-					
+			callback();					
 		}));
 	},
 
 	refresh: function() {
-		this.load_quote(this.pair, Lang.bind(this, function(txt) {
+		this.load_quote(this.pair, Lang.bind(this, function() {			
+			let txt;
+			if( current_quote.change > 0 )
+				txt = UP_POINTING + ' ' + current_quote.bid;
+			else
+				txt = DOWN_POINTING + ' ' + current_quote.bid;			
 			this.buttonText.set_text(txt);
 		}));
     	if(this.change_timeoutloop) {
@@ -136,7 +169,8 @@ const ForexIndicator = new Lang.Class({
    	},
 	
 	destroy: function(){
-      this.remove_timeout();
+		this.remove_timeout();
+		this.parent();		
 	},
 
 });
@@ -145,9 +179,10 @@ function init() {
 }
 
 function enable() {
-   forex_indicator_object=new ForexIndicator;
-   if(forex_indicator_object) {
-      Main.panel.addToStatusArea('forex-indicator',forex_indicator_object);
+	current_quote = new Quote;	
+	forex_indicator_object = new ForexIndicator;
+	if(forex_indicator_object) {
+		Main.panel.addToStatusArea('forex-indicator',forex_indicator_object);
    }
 }
 
