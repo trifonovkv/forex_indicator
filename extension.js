73b7c984ea354e9238886bd82acab044f4e8b22d
trifonovkv <trifonovkv@gmail.com>
2014-08-07 16:24:12 +0600

    Release 1.0

--- extension.js
+++ extension.js
@@ -6,163 +6,214 @@ const Mainloop=imports.mainloop;
 const Clutter=imports.gi.Clutter;
 const PanelMenu=imports.ui.panelMenu;
 const PopupMenu=imports.ui.popupMenu;
-//const Util = imports.misc.util;
-const Gettext = imports.gettext.domain('gnome-shell-extensions');
+const Util = imports.misc.util;
+const Gettext = imports.gettext.domain('gnome-shell-extension-forexindicator');
 const _ = Gettext.gettext;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Config = imports.misc.config;
+const Convenience = Me.imports.convenience;
 
-const SYMBOLS = [ 
-	'EURUSD', 
-	'GBPUSD', 
-	'USDJPY', 
-	'USDCHF', 
-	'USDCAD', 
-	'EURJPY',
-	'EURCHF',
-	'GBPJPY',
-	'GBPCHF',
-	'GOLD'
-];
+const FOREX_SETTINGS_SCHEMA = 'org.gnome.shell.extensions.forexindicator';
+const FOREX_PAIR_CURRENT = 'pair-current';
+const FOREX_REFRESH_INTERVAL = 'refresh-interval';
+const FOREX_PRICE_IN_PANEL = 'price-in-panel';
 
 const QUOTES_URL = 'http://quotes.instaforex.com/get_quotes.php';
 const UP_POINTING = String.fromCharCode(9650);
 const DOWN_POINTING = String.fromCharCode(9660);
 
-const Quote = new Lang.Class({
-	Name: 		'Quote',
-	symbol:		null,
-	ask:		0.0,
-	bid:		0.0,
-	change: 	0.0,
-	digits:		0,
-	lasttime:	0,
-	pair:		null,
-
-	load_quote: function(callback) {
-		let params = {m: 'json', q: null};
-		params.q=this.pair;
-		const _httpSession = new Soup.Session();
-  		let message = Soup.form_request_new_from_hash('GET', QUOTES_URL, params);
-		_httpSession.queue_message(message, Lang.bind(this, function(_httpSession, message) { 
-         	let json = JSON.parse(message.response_body.data);
-            for (let i in json) {
-				current_quote.symbol = json[i].symbol;
-				current_quote.ask = json[i].ask;
-				current_quote.bid = json[i].bid;
-				current_quote.change = json[i].change;
-				current_quote.digits = json[i].digits;
-				current_quote.lasttime = json[i].lastime;				
-			}
-			callback();					
-		}));
-	},
-});
+let _httpSession;
 
 const ForexIndicator = new Lang.Class({
-	Name: 'ForexIndicator',
-	Extends: PanelMenu.Button,
- 	buttonText: null,
-	timeout: null,
-	change_timeout_loop: false,
+    Name: 'ForexIndicator',
+    Extends: PanelMenu.Button,
 	
-	_init: function() {
-		this.parent(0.0,"Forex Indicator",false);
-		this.buttonText=new St.Label({
-         	name: "forex-indicator-buttonText",
-			text: _("Loading..."),
-         	y_align: Clutter.ActorAlign.CENTER
-     	});
-		
-		current_quote = new Quote;		
-	
-		this.actor.add_actor(this.buttonText);
-		this.actor.connect('button-press-event', Lang.bind(this, this.refresh));
-//      	this.actor.connect('key-press-event', Lang.bind(this, this.refresh));
+    _init: function() {
+        this._loadConfig();
+        this.parent(0.0, "Forex Indicator", false);
+        this.buttonText=new St.Label({ text: _("Loading..."),
+                                       y_align: Clutter.ActorAlign.CENTER});	
+        this.actor.add_actor(this.buttonText);
 
-		for (let i=0; i < SYMBOLS.length; i++) {
-	    	let symbol = SYMBOLS[i];
-	    	let item = new PopupMenu.PopupMenuItem(SYMBOLS[i]);
-	    	item.connect('activate', Lang.bind(this, function(){
-				current_quote.pair = symbol;
-				this.change_timeoutloop=true;
-				this.refresh();
-	    	}));
-	    	this.menu.addMenuItem(item);
-		}		
+        let label = new St.Label({ text: _("Symbol")});
+        this.symbol = new St.Label();
+        let item = new PopupMenu.PopupBaseMenuItem({ reactive: false});
+        item.actor.add(this.symbol, { expand: true });
+        item.actor.add(label);		
+        this.menu.addMenuItem(item);
 
-		let item = new PopupMenu.PopupSeparatorMenuItem();
+        let label = new St.Label({ text: _("Ask")});
+        this.ask = new St.Label();
+        let item = new PopupMenu.PopupBaseMenuItem({ reactive: false});
+        item.actor.add(this.ask, { expand: true });
+        item.actor.add(label);
+        this.menu.addMenuItem(item);
+
+        let label = new St.Label({ text: _("Bid")});
+        this.bid = new St.Label();
+        let item = new PopupMenu.PopupBaseMenuItem({ reactive: false});
+        item.actor.add(this.bid, { expand: true });
+        item.actor.add(label);
+        this.menu.addMenuItem(item);
+
+        let label = new St.Label({ text: _("Change")});
+        this.change = new St.Label();
+        let item = new PopupMenu.PopupBaseMenuItem({ reactive: false});
+        item.actor.add(this.change, { expand: true });
+        item.actor.add(label);
+        this.menu.addMenuItem(item);
+
+        this.lasttime = new St.Label();
+        let item = new PopupMenu.PopupBaseMenuItem({ reactive: false});
+        item.actor.add(this.lasttime);
+        this.menu.addMenuItem(item);
+
+        let item = new PopupMenu.PopupSeparatorMenuItem();
+        this.menu.addMenuItem(item);
+
+        let item = new PopupMenu.PopupMenuItem(_("Reload"));
+        item.connect('activate', Lang.bind(this, function() {
+            this._online_status = true;
+            this._refresh();
+        }));
         this.menu.addMenuItem(item);
 		
-//		let item = new PopupMenu.PopupMenuItem(_("Settings"));
-//        item.connect('activate', Lang.bind(this, this._onPreferencesActivate));
-//		this.menu.addMenuItem(item);
+        let item = new PopupMenu.PopupMenuItem(_("Settings"));
+        item.connect('activate', Lang.bind(this, this._onPreferencesActivate));
+        this.menu.addMenuItem(item);
 
-		let item = new PopupMenu.PopupMenuItem(_("Offline"));
-        item.connect('activate', Lang.bind(this, function(){
-			this.offline();
-		}));
-		this.menu.addMenuItem(item);
-		
-		this.change_timeoutloop=true;
-		this.refresh();
-	},
+        let item = new PopupMenu.PopupMenuItem(_("Offline / Online"));
+        item.connect('activate', Lang.bind(this, this._setOffline));
+        this.menu.addMenuItem(item);
 
-	refresh: function() {
-		current_quote.load_quote(Lang.bind(this, function() {			
-			let txt;
-			if( current_quote.change > 0 )
-				txt = UP_POINTING + ' ' + current_quote.bid;
-			else
-				txt = DOWN_POINTING + ' ' + current_quote.bid;			
-			this.buttonText.set_text(txt);
-		}));
-    	if(this.change_timeoutloop) {
-			this.remove_timeout();
-			this.timeout = Mainloop.timeout_add_seconds(30, Lang.bind(this, this.refresh));
-         	this.change_timeoutloop = false;
-         	return false;
-      	}
-      	return true;
-   	},
+        this._online_status = true;
+        this._refresh();
+    },
 
-	offline: function() {
-		this.remove_timeout();
-//		this.change_timeoutloop=false;
-		this.buttonText.set_text(_("Offline"));
-	},
+    _loadData: function(callback) {
+        let params = {m: 'json', q: this._currentPair};
+        _httpSession = new Soup.Session();
+        let message = Soup.form_request_new_from_hash('GET', QUOTES_URL, params);
+        _httpSession.queue_message(message, Lang.bind(this, function(_httpSession, message) { 
+            if (message.status_code !== 200)
+			    return;
+            let json = JSON.parse(message.response_body.data);
+            this._buildUI(json);					
+        }));
+    },
+
+    _refresh: function() {
+        if(this._online_status == false)
+            return false;
+        this._loadData();
+        this._removeTimeout();
+        this._timeout = Mainloop.timeout_add_seconds(this._refreshInterval, 
+                                                Lang.bind(this, this._refresh));
+        return true;
+    },
+
+    _setOffline: function() {
+        if(this._online_status) {
+            this._removeTimeout();
+            this.buttonText.set_text(_("Offline"));
+            this._online_status = false;
+        }
+        else {
+            this._refresh();
+            this._online_status = true;
+        }		
+    },
 	
-//	_onPreferencesActivate: function() {
-//        Util.spawn(["gnome-shell-extension-prefs", "forex_indicator@trifonovkv.gmail.com"]);
-//        return 0;
-//    },	
-	
-	remove_timeout: function() {
-    	if(this.timeout) {
-         	Mainloop.source_remove(this.timeout);
-         	this.timeout=null;
-      	}
-   	},
-	
-	destroy: function(){
-		this.remove_timeout();
-		this.parent();		
-	},
+    _onPreferencesActivate: function() {
+        Util.spawn(["gnome-shell-extension-prefs", "forex_indicator@trifonovkv.gmail.com"]);
+        return 0;
+    },	
 
+    _loadConfig: function() {
+        this._settings = Convenience.getSettings(FOREX_SETTINGS_SCHEMA);
+        this._settingsC = this._settings.connect("changed", Lang.bind(this, function() {
+            this._refresh();
+        }));
+    },
+
+    _buildUI: function(data) {
+        for (let i in data) {
+            this.symbol.set_text(data[i].symbol);
+            this.ask.set_text(data[i].ask);
+            this.bid.set_text(data[i].bid);
+            this.change.set_text(data[i].change);
+            let date = new Date((data[i].lasttime-10800)*1000);
+            this.lasttime.set_text(date.toLocaleString());
+        }
+
+        let txt;
+        if(this.change > 0)
+            txt = UP_POINTING;
+        else
+            txt = DOWN_POINTING;
+
+        if(this._priceInPanel == "Ask")
+            txt = txt + ' ' + this.ask.text;
+        else
+            txt = txt + ' ' + this.bid.text;
+			
+        this.buttonText.set_text(txt);
+    },
+
+    get _currentPair() {
+        if (!this._settings)
+            this._loadConfig();
+        return this._settings.get_string(FOREX_PAIR_CURRENT);
+    },		
+
+    get _refreshInterval() {
+        if (!this._settings)
+            this._loadConfig();
+        return this._settings.get_int(FOREX_REFRESH_INTERVAL);
+    },
+
+    get _priceInPanel() {
+        if (!this._settings)
+            this._loadConfig();
+        return this._settings.get_string(FOREX_PRICE_IN_PANEL);
+    },
+	
+    _removeTimeout: function() {
+        if(this._timeout) {
+            Mainloop.source_remove(this._timeout);
+            this._timeout=null;
+        }
+    },
+
+	stop: function() {
+        if (_httpSession !== undefined)
+            _httpSession.abort();
+        _httpSession = undefined;
+
+        if (this._timeout)
+            Mainloop.source_remove(this._timeout);
+        this._timeout = undefined;
+
+        if (this._settingsC) {
+            this._settings.disconnect(this._settingsC);
+            this._settingsC = undefined;
+        }
+    }
 });
 
+let forexMenu;
+
 function init() {
+    Convenience.initTranslations('gnome-shell-extension');
 }
 
 function enable() {	
-	forex_indicator_object = new ForexIndicator;
-	if(forex_indicator_object) {
-		Main.panel.addToStatusArea('forex-indicator',forex_indicator_object);
-   }
+    forexMenu = new ForexIndicator;
+    Main.panel.addToStatusArea('forex-indicator', forexMenu);
 }
 
-// Disable function
 function disable() {
-   if(forex_indicator_object) {
-      forex_indicator_object.destroy();
-      forex_indicator_object=null;
-   }
+    forexMenu.stop();
+    forexMenu.destroy();
 }
