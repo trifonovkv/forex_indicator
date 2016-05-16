e7c593dc5a181218009ba59e13be37eecb51e66a
trifonovkv <trifonovkv@gmail.com>
2016-05-16 14:11:17 +0500

    Fix time and assign data format

--- extension.js
+++ extension.js
@@ -25,6 +25,7 @@ const FOREX_ONLINE_STATUS = 'online-status';
 const QUOTES_URL = 'http://quotes.instaforex.com/get_quotes.php';
 const UP_POINTING = String.fromCharCode(9650);
 const DOWN_POINTING = String.fromCharCode(9660);
+const SERVER_TIME_GMT_DIFF = 10800;
 
 let _httpSession;
 
@@ -151,10 +152,10 @@ const ForexIndicator = new Lang.Class({
     _refreshUI: function(data) {
         for (let i in data) {
             this.symbol.set_text(data[i].symbol);
-            this.ask.set_text(data[i].ask);
-            this.bid.set_text(data[i].bid);
-            this.change.set_text(data[i].change);
-            let date = new Date((data[i].lasttime - 7200) * 1000);
+            this.ask.set_text(data[i].ask.toString());
+            this.bid.set_text(data[i].bid.toString());
+            this.change.set_text(data[i].change.toString());
+            let date = new Date((data[i].lasttime - SERVER_TIME_GMT_DIFF) * 1000);
             this.lasttime.set_text(date.toLocaleString());
         }
 
